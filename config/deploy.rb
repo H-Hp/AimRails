7f7bb67feaf4f6c68be7bcdea4ec9767d19d33a2
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-04-26 08:13:26 +0900

    deploy.rbの編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,21 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.17.2"
+lock "~> 3.17.2" # Capistranoのバージョン
+set :application, "Aim" #デプロイするアプリの名称
+set :repo_url, "git@github.com:H-Hp/AimRails.git" #リポジトリURL
+set :branch, 'main' #リポジトリのブランチ
+set :deploy_to, '/Aim' #デプロイ先のディレクトリ
+set :linked_files, %w(config/master.key) #シンポリックリンクを貼るファイル
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system) #シンポリックリンクを貼るディレクトリ
+set :keep_releases, 5 #保持するリリースバージョンの数
+set :rbenv_ruby, '2.7.3' #デプロイ先サーバーにインストールされているRubyバージョン
+set :log_level, :debug #出力するログレベル
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :default_env, { #デプロイ先サーバーに設定する環境変数
+  rbenv_root: "/home/user/.rbenv",
+  path: "/home/user/.rbenv/bin:$PATH"
+}
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+set :ssh_options, { # SSH接続設定
+  auth_methods: ['publickey'], 
+  #keys: ['~/.ssh/プライベートキーの名前.pem']
+  keys: ['/Users/hayashihiroki/Desktop/Aim/Rails_Aim/aim-mini-ec2-keypair.pem']
+}
