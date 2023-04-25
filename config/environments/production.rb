a51a903abe08236227bec9c6ffa08ea4cca273b7
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-04-26 04:18:45 +0900

    production-aim-bigの環境ファイル作成

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "aim-yw6r.onrender.com"
+  config.hosts << "aim-get.com"
+  config.hosts << "www.aim-get.com"
   
   # Code is not reloaded between requests.
   config.cache_classes = true
