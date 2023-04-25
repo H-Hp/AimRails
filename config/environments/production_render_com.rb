244953b513f8077cbb73a96af800f8471901abd8
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-04-25 11:16:25 +0900

    database.ymlにRender.comへの接続情報を編集

--- config/environments/production_render_com.rb
+++ config/environments/production_render_com.rb
@@ -2,4 +2,5 @@ require_relative "production"#production.rbの設定をオーバーライド
 Rails.application.configure do
   # production.rbを上書きする設定を記述
   config.hosts << "aim-rails.onrender.com"
+  config.hosts.clear #全てのhostを受け入れる
 end
