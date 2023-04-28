c667208a68d1c1c6d9a955f65236a6633747f1f1
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-04-28 12:39:03 +0900

    circleciからcapistranを起動させる設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,28 @@
 lock "~> 3.17.2" # Capistranoのバージョン
+
+
 set :application, "Aim" #デプロイするアプリの名称
 set :repo_url, "git@github.com:H-Hp/AimRails.git" #リポジトリURL
 set :branch, 'main' #リポジトリのブランチ
-set :deploy_to, '/Aim' #デプロイ先のディレクトリ
-set :linked_files, %w(config/master.key) #シンポリックリンクを貼るファイル
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system) #シンポリックリンクを貼るディレクトリ
+set :deploy_to, '/home/ec2-user/AimTest' #デプロイ先のディレクトリ
+#set :linked_files, %w(config/master.key) #シンポリックリンクを貼るファイル
+#set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system) #シンポリックリンクを貼るディレクトリ
 set :keep_releases, 5 #保持するリリースバージョンの数
-set :rbenv_ruby, '2.7.3' #デプロイ先サーバーにインストールされているRubyバージョン
+#set :rbenv_ruby, '2.7.3' #デプロイ先サーバーにインストールされているRubyバージョン
 set :log_level, :debug #出力するログレベル
 
-set :default_env, { #デプロイ先サーバーに設定する環境変数
-  rbenv_root: "/home/user/.rbenv",
-  path: "/home/user/.rbenv/bin:$PATH"
-}
+#set :default_env, { #デプロイ先サーバーに設定する環境変数
+  #rbenv_root: "/homeec2-user/AimTest/.rbenv",
+  #path: "/home/ec2-user/AimTest/.rbenv/bin:$PATH"
+#}
 
 set :ssh_options, { # SSH接続設定
-  auth_methods: ['publickey'], 
+  auth_methods: ['publickey'], #SSH接続の認証方法としてpublickey を指定
   #keys: ['~/.ssh/プライベートキーの名前.pem']
   keys: ['/Users/hayashihiroki/Desktop/Aim/Rails_Aim/aim-mini-ec2-keypair.pem']
+  #keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true #ローカル環境で管理しているSSH秘密鍵を中継できる・CircleCI上でSSHコマンドを実行する際に、ローカルで保持しているSSH鍵を使用して、EC2インスタンスにSSH接続することができます。つまり、CircleCIからローカルのSSH鍵を参照して、EC2にSSH接続できるようになります
 }
+=begin
+=end
+
--- config/deploy/production-aim-mini.rb
+++ config/deploy/production-aim-mini.rb
@@ -1,6 +1,52 @@
-server '12.345.67.89', user: 'ec2-user', roles: %w{app db web}
-
-#サンプル
+server '13.114.132.66', user: 'ec2-user', roles: %w{app db web}
 #server 'xxx.xxx.xxx.xxx',user: 'xxxxx',roles: %w{app db web},port: xxxxx,ssh_options: {keys: '~/.ssh/id_rsa'}
 #server 'Elastic IPアドレス', user: 'ec2-user', roles: %w{app db web}
-#server '12.345.67.89', user: 'ec2-user', roles: %w{app db web}
+
+#デプロイ前に実行するには、before 'deploy:starting', 'my_custom_task:run'という設定を
+#config/deploy.rbに追加することで実現できます。
+#この設定により、deploy:startingタスクが実行される前に、my_custom_task:runタスクが実行されます。
+before 'deploy:starting', 'my_custom_task:local_ImageBuild_dockerhub_push'
+before 'deploy:starting', 'my_custom_task:ec2_up'
+
+#カスタムタスクを呼び出す・デプロイの完了後に my_custom_task:run タスクが実行される
+#after 'deploy:finished', 'my_custom_task:run'
+#after 'deploy:finished', 'my_custom_task:ec2_up'
+
+namespace :my_custom_task do
+  desc "ローカルでDockerImageをビルドして作成し、タグ付けを行い、DockerHubへpush"
+  task :local_ImageBuild_dockerhub_push do
+    #on roles(:app) do
+      # カスタムタスクの内容
+      #execute "echo 'aa'"
+      #execute "echo 'pdw='; pwd"
+    #end
+    run_locally do
+      execute "echo 'ローカルでDockerImageをビルドして作成し、タグ付けを行い、DockerHubへpush'; pwd"
+      execute "docker-compose -f docker-compose-aim-mini.yml build"
+      execute "docker tag aim-mini-app-image:latest dockermy7777/aim:mini-app-latest"
+      #execute "docker tag aim-mini-web-image:latest dockermy7777/aim:mini-web-latest"
+      execute "docker push dockermy7777/aim:mini-app-latest"
+      #execute "docker push dockermy7777/aim:mini-web-latest"
+    end
+  end
+  desc "EC2での処理"
+  task :ec2_up do
+    on roles(:app) do
+      execute "echo 'EC2での処理・pdw='; pwd"
+      execute "docker pull dockermy7777/aim:mini-app-latest"
+      #execute "docker pull dockermy7777/aim:mini-web_latest"
+      #execute "cd /Aim"
+      #execute "pwd;ls -a"
+      #execute "docker-compose -f docker-compose-ec2-aim-mini.yml up -d"
+      execute "docker-compose -f /Aim/docker-compose-ec2-aim-mini.yml up -d"
+      #execute "docker-compose -f docker-compose-ec2-aim-mini.yml up"
+
+      #execute "docker exec -it 1d90abc06630 bash" #appコンテナに入る
+      #execute "docker exec -it aim_app_container bash" #appコンテナに入る
+      #execute "rails -version"
+      #execute "rails db:migrate"
+      #execute "exit" #appコンテナから抜ける
+      #execute "docker-compose -f docker-compose-ec2-aim-mini.yml restart"
+    end
+  end
+end
