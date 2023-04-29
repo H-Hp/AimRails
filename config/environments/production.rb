d97a10e97dc999e48d22448352df468203737681
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-04-29 17:35:28 +0900

    .circleci/configを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  #config.public_file_server.enabledがtrueに設定されている場合、Railsアプリケーションはプリコンパイルされたアセットファイルを静的ファイルとして提供
   #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   config.assets.compile = false
