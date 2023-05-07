a34faba4fc7256a66b58e920b9e2e5e271136a4e
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-05-07 21:10:09 +0900

    テスト変更

--- spec/requests/contact_request_spec.rb
+++ spec/requests/contact_request_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Contacts", type: :request do
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/contact/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  #describe "GET /new" do
+  #  it "returns http success" do
+  #    get "/contact/new"
+  #    expect(response).to have_http_status(:success)
+  #  end
+  #end
 
 
 end
