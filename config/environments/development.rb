d99fc4561aa345fd23331c3b72784a26b2efe37b
H-Hp <stepjump3333@yahoo.co.jp>
2023-08-03 12:30:06 +0900

    config.action_mailer.delivery_methodにsesを指定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { host: '127.0.0.1' }
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :ses # or :sesv2
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
