905edbc07f48ed5ef0d5f1a43500b6e845c22f7a
H-Hp <stepjump3333@yahoo.co.jp>
2023-12-13 08:57:37 +0900

    fix：sitemap編集

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -10,8 +10,8 @@ SitemapGenerator::Sitemap.create do
   
   add '/llm', :changefreq => 'monthly' , :priority => 0.7 , :lastmod => '2023-12-12T10:51:23+09:00'
 
-  latest_playChat_updated = Play.order(created_at: :desc).first
-  add '/play', :changefreq => 'monthly' , :priority => 0.7 ,:lastmod => latest_playChat_updated.updated_at
+  latest_playChat_created_at = Play.order(created_at: :desc).first
+  add '/play', :changefreq => 'monthly' , :priority => 0.7 ,:lastmod => latest_playChat_created_at.created_at
   
   # 各投稿をsitemapに追加する
   Aim.all.each do |aim|
