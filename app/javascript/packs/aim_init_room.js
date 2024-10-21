697018b990fc1f1389d6d13eabdbb215b36addde
H-Hp <stepjump3333@yahoo.co.jp>
2024-10-21 18:41:07 +0900

    feat:AimRoomのreactやphaser.jsをapp/javascript/packs/aim_init_room.jsから読み込むようにする

--- app/javascript/packs/aim_init_room.js
+++ app/javascript/packs/aim_init_room.js
@@ -1,22 +1,10 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
-/*import React from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AimRoom from "../aim_room/components/AimRoom"
-
 //Webpackerが処理できるように画像をインポートする Import images so Webpacker processes them
 require.context('../../assets/images', true)
-
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.body.appendChild(document.createElement('div'))
   const root = createRoot(container)
   root.render(<AimRoom />)
 })
-*/
