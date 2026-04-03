f5b175c5adf75de519ed8e6ea3c717ee98d2eda2
H-Hp <stepjump3333@yahoo.co.jp>
2026-04-04 05:53:46 +0900

    feat: helthチェックのcors設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,3 +1,13 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "https://aegis-mysite-checker.vercel.app" #許可するOrigin・完全一致が必要
+    resource "/health_check", #許可するエンドポイント・ここだけ許可
+      headers: :any,
+      methods: [:get], #不要なPOST/PUTなど抜く
+      max_age: 600 # プリフライトキャッシュ（10分）・パフォーマンス向上
+  end
+end
+
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
